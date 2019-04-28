e510397966b0198e57f49e2d2850871d23170a2b
Espen Ogino Rathe <espenrathe@gmail.com>
2019-04-28 20:18:05 +0200

    Add router.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import './features/map/MapScreen';
+import MapScreen from './features/map/MapScreen';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 style={{ color: 'red' }} className="App-title">
-            {this.props.map.title}
-          </h1>
-        </header>
-        <p className="App-intro" />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 style={{ color: 'white' }} className="App-title">
+              {this.props.map.title}
+            </h1>
+            <Link to="/map" className="App-link">
+              Map
+            </Link>
+            <Link to="/list" className="App-link">
+              List
+            </Link>
+            <Link to="/add" className="App-link">
+              Add
+            </Link>
+            <Link to="/profile" className="App-link">
+              Profile
+            </Link>
+          </header>
+          <body>
+            <Route path="/map" component={MapScreen} />
+            <Route path="/list" component={List} />
+            <Route path="/add" component={Add} />
+            <Route path="/profile" component={Profile} />
+          </body>
+        </div>
+      </Router>
     );
   }
 }
+
+const List = () => {
+  return <p className="App-intro">Listlist</p>;
+};
+
+const Add = () => {
+  return <p className="App-intro">Addadd</p>;
+};
+
+const Profile = () => {
+  return <p className="App-intro">Profilefile</p>;
+};
 
 const mapStateToProps = state => ({
   ...state
